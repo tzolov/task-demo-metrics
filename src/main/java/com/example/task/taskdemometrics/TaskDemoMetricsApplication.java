bc04e35a1a9669e018c9625e66e56474c57fd888
Christian Tzolov <christian.tzolov@gmail.com>
2019-07-09 00:18:26 +0200

    initial commit

---
+++ src/main/java/com/example/task/taskdemometrics/TaskDemoMetricsApplication.java
@@ -0,0 +1,40 @@
+package com.example.task.taskdemometrics;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TaskDemoMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskDemoMetricsApplication.class, args);
+	}
+
+	@Bean
+	public TimestampTask timeStampTask() {
+		return new TimestampTask();
+	}
+
+	/**
+	 * A commandline runner that prints a timestamp.
+	 */
+	public class TimestampTask implements CommandLineRunner {
+		private final Log logger = LogFactory.getLog(TimestampTask.class);
+
+
+		@Override
+		public void run(String... strings) {
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+			logger.info(dateFormat.format(new Date()));
+		}
+	}
+}
---
+++ src/test/java/com/example/task/taskdemometrics/TaskDemoMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task.taskdemometrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskDemoMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
